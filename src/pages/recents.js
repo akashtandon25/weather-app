d45081ab83269d0a198a43e5b7b6cb587f25aa79
Akash Tandon <akashtandon2511@gmail.com>
2023-05-26 17:13:07 +0530

    Update recents.js

--- src/pages/recents.js
+++ src/pages/recents.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Recents=()=>{
     return (
-        <div>
-
+        <div className="bg-gray-100 font-semibold m-24 flex justify-center w-fit text-4xl">
+            Coming Soon...
         </div>
     )
 }
